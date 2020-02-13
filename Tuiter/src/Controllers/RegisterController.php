599743a408485e60e37c91a8761ec17de7e25621
edu <eduardokloster96@gmail.com>
2020-02-13 09:21:51 -0300

    ultimo commit

---
+++ Tuiter/src/Controllers/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tuiter\Controllers;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class RegisterController implements \Tuiter\Interfaces\Controller {
+    
+    public function config($app) {
+       
+        $app->get('/prueba', function (Request $request, Response $response, array $args) {
+            $template = $request->getAttribute("twig")->load('prueba.html');
+
+            $response->getBody()->write(
+                $template->render(['name' => 'Edus'])
+            );
+            return $response;
+
+
+        });
+    }
+}
